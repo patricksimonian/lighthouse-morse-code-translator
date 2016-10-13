759b4fba5d34799c7745f93bcda5b5f3fe07803c
Patrick Simonian <patricksimonian@gmail.com>
2016-10-13 10:08:28 -0700

    refactored into array

--- morse.js
+++ morse.js
@@ -1,36 +1,17 @@
 var args = process.argv.slice(2);
-const string = args.join(" ").toLowerCase();
-//enter any amount of arguments in terminal window
+const argsArray = args.join(" ").split("");
 
-function morseConverter(stringIn, morseCallback) {
-  //check if stringIn is being entered as an array,
-  //if process.argv is not being utilized
-  if(stringIn instanceof Array) {
-    stringIn = stringIn.join(" ");
-  }
-  //error handle incase future use of module
-  // gives possibility for incorrect type insertion
-  if(typeof stringIn === "string") {
-    convertedString = "";
-    for(let i = 0; i < stringIn.length; i++){
-      let currentChar = stringIn.charAt(i);
-      convertedString += morseCallback(currentChar);
-    }
-    return convertedString;
-  } else {
-    return "Please Enter Valid String";
-  }
-}
-
-function findMorseMatch(charIn) {
+//takes in array of values and converts valid characters into their respective morse
+//code, if no valid character exists the original character is returned
+function morseConverter(arrayIn) {
   const morseLib = require('./morse-library');
-  //check to see if morseLib currently contains that character
-  //return morse equivalent if true
-  if (morseLib.hasOwnProperty(charIn)) {
-    return morseLib[charIn];
-  }
-  //else return an unsupported char
-  return charIn;
+    convertedStringArray = arrayIn.map((charIn) => {
+      if (morseLib.hasOwnProperty(charIn)) {
+        return morseLib[charIn];
+      }
+      return charIn
+    });
+    return convertedStringArray.join("");
 }
-console.log(morseConverter(string, findMorseMatch));
-console.log(typeof "Asdfas");
+
+console.log(morseConverter(argsArray));
